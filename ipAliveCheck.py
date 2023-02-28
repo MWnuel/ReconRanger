7679509360340371944e8f0f20e73440af4e0fd6
LukasMoz <39192552+LukasMoz@users.noreply.github.com>
2023-02-28 18:46:28 +0100

    add automation

--- ipAliveCheck.py
+++ ipAliveCheck.py
@@ -10,19 +10,27 @@ conn = psycopg2.connect(
     user="USER",
     password="PW"
 )
-ip_range = 'IP_RANGE'
+cur = conn.cursor()
+
+cur.execute("SELECT ip_range FROM ip_ranges")
+rows = cur.fetchall()
+for row in rows:
+    if ":" not in str(row[0]):      #Ignore IPv6
+        if "137.138" not in str(row[0]):        #Already in DB
+            print(row[0])
+            ip_range = str(row[0])
+            network = ipaddress.IPv4Network(ip_range)
+            for ip in network:
+                is_alive = False
+                if "Host is up" in subprocess.check_output(["nmap","-PE", "-PP", "-p T:443,80,8443,22,3389,8449,8000,554,8089,1934,8080,873,7999", str(ip)]).decode("utf-8"):
+                    is_alive = True
+
+                query = "INSERT INTO ip (ip, ip_range, is_alive, last_check) VALUES (%s, %s, %s, %s)"
+                values = (str(ip), ip_range, is_alive , datetime.now())
+                cur.execute(query, values)
+                conn.commit()
 
 
-network = ipaddress.IPv4Network('ip_range')
-
-for ip in network:
-    is_alive = False
-    if "Host is up" in subprocess.check_output(["nmap","-PE", "-PP", "-p T:443,80,8443,22,3389,8449,8000,554,8089,1934,8080,873,7999", str(ip)]).decode("utf-8"):
-        is_alive = True
-
-    query = "INSERT INTO ip (ip, ip_range, is_alive, last_check) VALUES (%s, %s, %s, %s)"
-    values = (ip, ip_range, is_alive , datetime.now())
-    cur.execute(query, values)
-
-conn.commit()
 conn.close()
+
+print('Script finish')
