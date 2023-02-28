a3a100955ec19a4180f5ddcbd0f1ad8c8fa8eab8
MWnuel <82886781+MWnuel@users.noreply.github.com>
2023-02-28 17:55:27 +0100

    is_alive Check

--- ipAliveCheck.py
+++ ipAliveCheck.py
@@ -1,6 +1,7 @@
 import psycopg2
 import ipaddress
 from datetime import datetime
+import subprocess
 
 conn = psycopg2.connect(
     host="IP",
@@ -16,7 +17,9 @@ network = ipaddress.IPv4Network('ip_range')
 
 for ip in network:
     is_alive = False
-    ##CHECK IF IP is Alive
+    if "Host is up" in subprocess.check_output(["nmap","-PE", "-PP", "-p T:443,80,8443,22,3389,8449,8000,554,8089,1934,8080,873,7999", str(ip)]).decode("utf-8"):
+        is_alive = True
+
     query = "INSERT INTO ip (ip, ip_range, is_alive, last_check) VALUES (%s, %s, %s, %s)"
     values = (ip, ip_range, is_alive , datetime.now())
     cur.execute(query, values)
